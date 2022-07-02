f40299f81b066b84b16b020ac08ba29bf6311251
Artem Makhov <makhovartem23@gmail.com>
2022-07-02 20:40:01 +0300

    add logic withaut styles

--- src/index.js
+++ src/index.js
@@ -3,28 +3,41 @@
 import { fetchImages , resetPages} from "./js/fetch-images";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import throttle from "lodash.throttle";
+import { Notify } from "notiflix";
 
-const form = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
 
-form.addEventListener('submit', onFormSubmit);
-loadMoreBtn.addEventListener('click',onClickLoadMoreBtn);
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  scrollGuard:document.querySelector('.scroll-guard'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt', captionsDelay: 250,
 })
+
 let searchText = '';
+let lastPage = 0;
+
 async function onFormSubmit(e) {
     e.preventDefault();
     clearCardsContainer();
     resetPages();
     searchText = e.currentTarget.searchQuery.value.trim();
-    console.log(searchText);
-    const {hits} = await fetchImages(searchText);
     
+    const {totalHits,hits} = await fetchImages(searchText);
+    // console.log();
     e.target.reset();
-    if (hits.length === 0) {
-        console.log("dsfd")
+  
+  
+  if (hits.length === 0) {
+    alertNoImagesFound();
+  } else {
+        alertYesImagesFound(totalHits);
     }
 
     renderCards(hits);
@@ -32,13 +45,11 @@ async function onFormSubmit(e) {
 }
 
 
-function renderCards(cards) {
-    gallery.insertAdjacentElement('beforeend',createCardsMarkup(cards))
-}
+
 
 function createCardsMarkup(cards) {
-    return cards.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-        return `<div class="photo-card">
+    return cards.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+       `<div class="photo-card">
         <a href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
@@ -57,18 +68,63 @@ function createCardsMarkup(cards) {
   </div>
   </a>
 </div>`
-    }).join('');
+    ).join('');
+}
+
+function renderCards(cards) {
+  refs.gallery.insertAdjacentHTML('beforeend', createCardsMarkup(cards));
 }
 
 
 function clearCardsContainer() {
-    gallery.innerHTML = '';
+    refs.gallery.innerHTML = '';
 }
 
-async function onClickLoadMoreBtn() {
+async function onLoadMoreImg() {
     const { hits } = await fetchImages(searchText);
     renderCards(hits);
     lightbox.refresh();
 }
 
+// function onClickTopBtn() {
+//   if (window.pageYOffset > 0) {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   }
+// }
+
+function alertNoImagesFound() {
+  Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function alertYesImagesFound(hits) {
+  Notify.success(`Hooray! We found ${hits} images.`);
+}
+
+
+const options = {
+  rootMargin: "500px",
+  threshold: 1.0,
+};
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+   
+   if (searchText === '') {
+      return;
+    }
+    console.log( );
+    //  if (page === lastPage) {
+    //   setTimeout(() => {
+    //     Notify.info("We're sorry, but you've reached the end of search results.");
+    //   }, 1000);
+    //   lastPage = 0;
+    //   return;
+    // }
+
+    if (entry.isIntersecting) {
+       onLoadMoreImg();
+      }
+  });
+}, options);
+ 
+observer.observe(refs.scrollGuard);
 
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { Notify } from "notiflix";
+
 export { fetchImages, resetPages }
 
 
@@ -11,6 +13,7 @@ const KEY_API = '28371494-c5e29ef795b67968f0a2006d4';
 let page = 1;
 
 async function fetchImages(query) {
+    
     const optionParam = new URLSearchParams({
         key: `${KEY_API}`,
         q: query,
@@ -19,15 +22,26 @@ async function fetchImages(query) {
         safesoarch: 'true',
         page: page,
         per_page: 40,
-    })
+    });
 
     const{data} = await axios.get(
         `?${optionParam}`
     );
+    
     page += 1;
-    return data;
-}
+    
+    let lastPage = data.totalHits - 40 * page;
+    console.log(lastPage);
+    if ( lastPage <= 0 ) {
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+      return;
+    }
+
+     return data;
+};
 
 function resetPages() {
     page = 1;
-}
+};
+
+
